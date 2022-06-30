214bdc8d8d74b530c5212f97f61a2d252c227c3a
edusal100 <edusal100@hotmail.com>
2022-06-29 22:20:03 -0500

    Item detail spacing

--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ItemCount from './ItemCount';
 
 export default function ItemDetail({product, stock}) {
-  console.log(product.brand);
+  
   return (
     <div className='flex mt-8 gap-8'>
        <div>
           <img className='w-80' src={product.image} alt="" />
        </div>
-       <div>
-          <h2 className='font-bold text-xl '>{product.model}</h2>
-          <h3 className='font-medium text-slate-500'>${product.price}</h3>
-          <p>{product.description}</p>
+       <div className=' w-80'>
+          <h2 className='font-bold text-xl mb-2'>{product.model}</h2>
+          <h3 className='font-medium text-slate-500 mb-2'>${product.price}</h3>
+          <p className='mb-8'>{product.description}</p>
           <ItemCount stock={15} initial={1}/>
        </div>
     </div>
