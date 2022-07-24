07ab4b9b7779ebc581a25e24d22ea4bd48953152
edusal100 <edusal100@hotmail.com>
2022-07-24 10:25:39 -0500

    Formbuyer and Itemcount ui changes

--- src/Components/FormBuyer.jsx
+++ src/Components/FormBuyer.jsx
@@ -71,24 +71,35 @@ export default function FormBuyer() {
     <div>
         {orderId ? 
 
-        <div className='flex justify-center'>
-            <h1>Thanks!</h1>
-            <h3>Your order is complete.</h3>
+        <div className="bg-[url('https://source.unsplash.com/mTkXSSScrzw')] bg-cover">
+            <div className='flex flex-col items-center py-40'>
+            <h1 className='font-extrabold text-2xl'>Thanks!</h1>
+            <h3 className='font-extrabold text-2xl'>Your order is complete.</h3>
             <p>El id de tu compra es {orderId}</p>
             <Link to="/">
             <button className='mt-8 h-10 px-6 font-semibold rounded-md bg-black text-white' >Home Page</button>
             </Link>
+            </div>
         </div> :
 
         <div>
+            <div className='bg-slate-100 text-center py-10 lg:py-14 mb-4 text-4xl font-extrabold'>Checkout</div>
             <form onChange={handleChange} onSubmit={createOrder}>
-                <label>Name</label>
-                <input type="name" name="name" value={formData.name} placeholder="" required/>
-                <label>Phone</label>
-                <input type="text" name="phone" value={formData.phone} placeholder="" required/>
-                <label>Email</label>
-                <input type="email" name="email" value={formData.email} placeholder="" required/>
-                <button className='mt-8 h-10 px-6 font-semibold rounded-md bg-black text-white' >Proceed to Payment</button>
+                <div className='lg:flex gap-4 mt-10'>
+                <div className='block'>
+                <label className='block'>Name</label>
+                <input className='border px-3 py-2' type="name" name="name" value={formData.name} placeholder="" required/>
+                </div>
+                <div className='block'>     
+                <label  className='block'>Phone</label>
+                <input className='border px-3 py-2' type="text" name="phone" value={formData.phone} placeholder="" required/>
+                </div>
+                <div className='block'>  
+                <label className='block'>Email</label>
+                <input className='border px-3 py-2' type="email" name="email" value={formData.email} placeholder="" required/>
+                </div>
+                </div>
+                <button className='mt-6 h-10 px-6 font-semibold rounded-md bg-black text-white' >Proceed to Payment</button>
             </form>
         </div>
         
